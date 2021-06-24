8d1f999df08090623a87100024339cec5a14165e
isaacf04 <isaaf04@gmail.com>
2021-06-23 19:03:20 -0600

    added prettier

--- test/specs/specialist.js
+++ test/specs/specialist.js
@@ -12,9 +12,9 @@ describe('Specialist', () => {
 
     it('Valdate the correct request after visiting specialist', async () => {
         await HomePage.open();
-        await specialistPage.open('/#/specialist/bfea3295-af20-4824-8bed-170a227b%20c1e6'); 
-        await browser.pause(1000);         
-        await specialistPage.verifySpecialistRequest();
+           await specialistPage.open('/#/specialist/bfea3295-af20-4824-8bed-170a227b%20c1e6'); 
+          await browser.pause(1000);         
+          await specialistPage.verifySpecialistRequest();
       
           
     });
